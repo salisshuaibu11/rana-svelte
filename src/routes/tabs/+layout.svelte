aa32c215eefc0a349508c001b9de0df531af73ad
Salisu Shuaibu <salisshuaibu11@gmail.com>
2023-02-19 23:15:27 +0100

    crated tabs

---
+++ src/routes/tabs/+layout.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import {IonTabs} from "ionic-svelte";
+
+  import { home, star, person} from "ionicons/icons";
+
+  const myTabs = [
+    {
+      label: 'Home',
+      icon: home,
+      tab: 'home',
+    },
+    {
+      label: 'Favorites',
+      icon: star,
+      tab: 'favorites',
+    },
+    {
+      label: 'Profile',
+      icon: person,
+      tab: 'profile',
+    }
+  ];
+
+  const logStuff = () => {
+    console.log('id');
+  }
+</script>
+
+<IonTabs slot="bottom" tabs={myTabs} ionTabsWillChange={logStuff} ionTabsDidChange={logStuff}>
+  <slot />
+</IonTabs>
