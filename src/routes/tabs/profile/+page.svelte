fb2890d7731dc47edcb215450da16bc34fa7296e
Salisu Shuaibu <salisshuaibu11@gmail.com>
2023-02-20 00:46:48 +0100

    profile page

--- src/routes/tabs/profile/+page.svelte
+++ src/routes/tabs/profile/+page.svelte
@@ -1,1 +1,40 @@
-Profile
+<ion-header>
+  <ion-toolbar>
+    <ion-title>
+      Profile
+    </ion-title>
+  </ion-toolbar>
+</ion-header>
+
+<ion-content>
+  <ion-avatar class="profile-pic">
+    <ion-img src="https://pbs.twimg.com/profile_images/1593690565794832390/NVdhJ1fD_400x400.jpg"></ion-img>
+  </ion-avatar>
+
+  <ion-card>
+    <ion-card-header>
+      <ion-card-title>Salisu Shuaibu</ion-card-title>
+    </ion-card-header>
+
+    <ion-card-content>
+      Learning Ionic By Sani Yusuf
+    </ion-card-content>
+
+    <ion-card-content>
+      Tech stack used in building this apps:
+      <ul>
+        <li>Sveltekit</li>
+        <li>Ionic</li>
+        <li>Firebase</li>
+      </ul>
+    </ion-card-content>
+  </ion-card>
+</ion-content>
+
+<style>
+.profile-pic {
+  width: 200px;
+  height: auto;
+  margin: 10px auto;
+}
+</style>
